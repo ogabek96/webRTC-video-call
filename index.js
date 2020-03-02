d2c45232a747a94e531c4d7bd2c96e58868a96e3
Ogabek Yusupov <ogabekyusupov@gmail.com>
2020-03-02 11:57:23 +0500

    index page added

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const express = require("express");
+const socket = require("socket.io");
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+const server = http.createServer(app);
+
+const io = socket(server);
+io.on("connection", connection => {
+  console.log("A new client connected");
+  connection.on("update", data => {
+    connection.broadcast.emit("update", data);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
+});
